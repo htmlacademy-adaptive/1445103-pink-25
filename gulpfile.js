13bbd364ff8396179e674b6de81f4d9e790507e4
Valeria <lerika.konstantinova@gmail.com>
2022-05-13 01:32:51 +1000

    Watcher

--- gulpfile.js
+++ gulpfile.js
@@ -120,7 +120,8 @@ const reload = (done) => {
 
 const watcher = () => {
   gulp.watch('source/less/**/*.less', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
+  //gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
 // Build
