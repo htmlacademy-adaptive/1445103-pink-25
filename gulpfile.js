d2ee2325975b5bdf66f7060e39c3d3b8d24e8a2a
Valeria <lerika.konstantinova@gmail.com>
2022-05-13 23:33:39 +1000

    jpeg

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ const copyImages = () => {
 // WebP
 
 const createWebp = () => {
-  return gulp.src('source/img/**/*.{png,jpg}')
+  return gulp.src('source/img/**/*.{png,jpg,jpeg}')
   .pipe(squoosh({
   webp: {}
   }))
